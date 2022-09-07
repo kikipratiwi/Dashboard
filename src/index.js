249a08fa67498d9d1c851a16ca7d03837b3de527
Kiki Pratiwi <kikipratiwi@gmail.com>
2022-09-08 00:09:59 +0700

    config(theme): add theme provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Provider} from 'react-redux'
+import {ThemeProvider} from '@emotion/react'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-
-import store from './app/store'
+import store from './redux/store'
+import theme from './themeConfig'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
         </Provider>
     </React.StrictMode>,
 )
---
+++ src/themeConfig.js
@@ -0,0 +1,15 @@
+import {createTheme} from '@mui/material/styles'
+import {green, purple} from '@mui/material/colors'
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#faf7f3',
+        },
+        secondary: {
+            main: '#fefcfa',
+        },
+    },
+})
+
+export default theme
