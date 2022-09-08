2ed755155ada321911089ce1c5ef49d219997b25
Kiki Pratiwi <kikipratiwi@gmail.com>
2022-09-08 12:59:04 +0700

    utils(toTitleCase): add remove special char utility

--- src/utils/toTitleCase.js
+++ src/utils/toTitleCase.js
@@ -1,15 +1,8 @@
 const toTitleCase = (str = ' ') => {
-    // Remove title prefix
-    const text =
-        str.includes('_') || str.includes('-')
-            ? str
-                  .replace(/[^a-zA-Z0-9]/g, ' ')
-                  .split(' ')
-                  .slice(1)
-                  .join(' ')
-            : str
+    // Remove special char
+    const text = str.replace(/[^a-zA-Z0-9]/g, ' ')
 
-    return text.replace(/\w\S*/g, function (txt) {
+    return text.replace(/\w\S*/g, (txt) => {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
     })
 }
